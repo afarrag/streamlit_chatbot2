ddaaa6135aa8a8a15827cfa49683a8c46899cefd
Ahmed M. Farrag <afarrag@users.noreply.github.com>
2024-09-28 05:35:07 -0400

    Adding `memory` and `chain` to session

--- rag_app.py
+++ rag_app.py
@@ -28,15 +28,15 @@ vector_db = FAISS.load_local("./faiss_index", embeddings, allow_dangerous_deseri
 retriever = vector_db.as_retriever(search_kwargs={"k": 2})
 
 # memory
-@st.cache_resource
 def init_memory(_llm):
     return ConversationBufferMemory(
         llm=llm,
         output_key='answer',
         memory_key='chat_history',
         return_messages=True)
-memory = init_memory(llm)
-
+  
+if "memory" not in st.session_state:
+    st.session_state.memory = init_memory(llm)
 # prompt
 template = """You are a nice chatbot having a conversation with a human. Answer the question based only on the following context and previous conversation. Keep your answers short and succinct.
 
@@ -53,12 +53,12 @@ prompt = PromptTemplate(template=template,
                         input_variables=["context", "question"])
 
 # chain
-chain = ConversationalRetrievalChain.from_llm(llm,
+if "chain" not in st.session_state:
+    st.session_state.chain = ConversationalRetrievalChain.from_llm(llm,
                                               retriever=retriever,
-                                              memory=memory,
+                                              memory=st.session_state.memory,
                                               return_source_documents=True,
                                               combine_docs_chain_kwargs={"prompt": prompt})
-
 
 ##### streamlit #####
 
@@ -87,7 +87,7 @@ if prompt := st.chat_input("Curious minds wanted!"):
     with st.spinner("Going down the rabbithole for answers..."):
 
         # send question to chain to get answer
-        answer = chain(prompt)
+        answer = st.session_state.chain(prompt)
 
         # extract answer from dictionary returned by chain
         response = answer["answer"]
